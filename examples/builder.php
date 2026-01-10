6f92d1511447e97dc9eb4f9d6063a8214d97bc99
thomascorthals <thomas_corthals@hotmail.com>
2026-01-10 21:31:25 +0100

    Builder

---
+++ examples/builder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Solarium\Builder\Analytics\FunctionBuilder;
+
+// ...
+
+$query = $client->createSelect();
+$analytics = $query->getAnalytics();
+
+$expr = FunctionBuilder::expr();
+$builder = FunctionBuilder::create()
+    ->where($expr->div(
+        $expr->sum(
+            'a',
+            $expr->fillMissing('b', 0)
+        ),
+        $expr->add(
+            10.5,
+            $expr->count(
+                $expr->mult('a', 'c')
+            )
+        )
+    ))
+;
+
+$analytics
+    ->addFunction('sale()', (string) $builder->getFunction())
+;
+
+// which would be equal to
+$analytics
+    ->addFunction('sale()', 'div(sum(a,fill_missing(b,0)),add(10.5,count(mult(a,c))))')
+; 
